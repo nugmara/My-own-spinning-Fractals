80a142600acb02378cd15cc62e6ba417a09a181e
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-07 16:50:08 +0200

    Testing

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 console.log(ctx);
-canvas.width = 600;
-canvas.height = 600;
-ctx.strokeStyle = "black";
+canvas.width = 900;
+canvas.height = 900;
+// ctx.strokeStyle = "black";
 ctx.lineWidth = 2;
-ctx.lineCap = "round"
+ctx.lineCap = "round";
 ctx.shadowColor = "black";
 ctx.shadowOffSetY = 10;
 ctx.shadowOffSetX = 5;
@@ -17,13 +17,14 @@ class Fractle {
     this.canvasHeight = canvasHeight;
     this.size = canvasWidth * 0.25;
     this.sides = 25;
-    this.maxLevel = 1;
-    this.scale = 0.6;
-    this.spread = 0.9;
+    this.maxLevel = 4;
+    this.scale = 0.7;
+    this.spread = Math.random() * 6 + 2;
     this.branches = 1;
-    
+    this.color = "hsl(" + Math.random() * 360 + ", 75%, 50%)";
   }
   draw(context) {
+    context.strokeStyle = this.color;
     context.save();
     context.translate(this.canvasWidth / 2, this.canvasHeight / 2);
     for (let i = 0; i < this.sides; i++) {
@@ -37,26 +38,28 @@ class Fractle {
     context.beginPath();
     context.moveTo(0, 0);
     context.lineTo(this.size, 0);
-    context.bezierCurveTo(20, 100, 200, 100, 200, 20);
+    context.arc(this.size * 1.5, 0, 50, 0, Math.PI * 2);
+    context.fill();
+    // context.strokeRect(this.size * 1.2, 0, 50, 50);
+    // context.bezierCurveTo(20, 100, 200, 100, 200, 20);
     context.stroke();
-    
+
     for (let i = 0; i < this.branches; i++) {
-        // We will draw our fractal, making this private drawLine method call iself over and over: basically Recursion.
-        context.save()
-        context.translate(this.size - (this.size/this.branches) * i, 0)
-        context.scale(this.scale, this.scale)
-    
-        context.save();
-        context.rotate(this.spread)
-        this.#drawLine(context, level + 1);
-        context.restore();
-    
-        context.save();
-        context.rotate(-this.spread)
-        this.#drawLine(context, level + 1);
-        context.restore();
-        context.restore()
-    
+      // We will draw our fractal, making this private drawLine method call iself over and over: basically Recursion.
+      context.save();
+      context.translate(this.size - (this.size / this.branches) * i, 0);
+      context.scale(this.scale, this.scale);
+
+      context.save();
+      context.rotate(this.spread);
+      this.#drawLine(context, level + 1);
+      context.restore();
+
+      context.save();
+      context.rotate(-this.spread);
+      this.#drawLine(context, level + 1);
+      context.restore();
+      context.restore();
     }
   }
 }
