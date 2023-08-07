56a54e7f5291a5c571ffd81b2c7c44d35cb8aa9e
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-07 13:42:02 +0200

    Create drawLine private method

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ console.log(ctx);
 canvas.width = 600;
 canvas.height = 600;
 ctx.strokeStyle = "black";
+ctx.lineWidth = 20;
+ctx.lineCap = "round";
 
 class Fractle {
   constructor(canvasWidth, canvasHeight) {
     this.canvasWidth = canvasWidth;
     this.canvasHeight = canvasHeight;
-    this.size = canvasWidth * 0.15;
-    
-}
+    this.size = canvasWidth * 0.25;
+    this.sides = 1;
+  }
   draw(context) {
-    context.translate(this.canvasWidth/2, this.canvasHeight/2)
+    context.save();
+    context.translate(this.canvasWidth / 2, this.canvasHeight / 2);
+    for (let i = 0; i < this.sides; i++) {
+      this.#drawLine(context);
+      context.rotate((Math.PI * 2) / this.sides);
+    }
+    context.restore();
+  }
+  #drawLine(context, level) {
     context.beginPath();
     context.moveTo(0, 0);
     context.lineTo(this.size, 0);
     context.stroke();
+    // We wiill draw our fractal, making this private drawLine method call iself over and over: basically Recursion.
+    context.save();
+    // context.rotate(1)
+    this.#drawLine(context);
+    context.restore();
   }
 }
 
